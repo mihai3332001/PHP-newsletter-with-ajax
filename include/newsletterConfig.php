f0141bf3efe676c9aa8a37b57603e5a3bf61a8f3
mihai3332001 <34579013+mihai3332001@users.noreply.github.com>
2018-08-14 19:56:51 +0300

    Update newsletterConfig.php

--- include/newsletterConfig.php
+++ include/newsletterConfig.php
@@ -2,9 +2,9 @@
 include('load.php');
 
 class newsletterConfig {
- 	public $host = "localhost";
- 	public $username = "djmichael";
- 	public $password = "t3ad0rmmdo";
- 	public $database = "newsletter_uic";
+ 	public $host = "";
+ 	public $username = "";
+ 	public $password = "";
+ 	public $database = "";
 }
 ?>
